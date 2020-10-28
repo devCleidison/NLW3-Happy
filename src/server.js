69de5e49f00173ba5412a0d1d8fd00b26b5bd1b8
Cleidison Silva <cleidison.dev@gmail.com>
2020-10-28 08:56:56 -0300

    Instalando e configurando o Template Engine - HBS

--- src/server.js
+++ src/server.js
@@ -5,12 +5,17 @@ const path = require('path');
 // iniciando o express
 const server = express()
 server
-// utilizando os arquivos estáticos
-.use(express.static('public'))
-// criar uma rota
-.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+    // utilizando os arquivos estáticos
+    .use(express.static('public'))
+
+    // configurar template engine
+    .set('views', path.join(__dirname, "view"))
+    .set('view engine', 'hbs')
+
+    // criar uma rota
+    .get('/', (request, response) => {
+        return response.render('index')
+    })
 
 // ligar o servidor
 server.listen(5500)
